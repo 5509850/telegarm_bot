bd25bc9017c1e04af6c42a2c4907ac08e552fd80
Aliaksandr_Harbunou <Aliaksandr_Harbunou@epam.com>
2016-11-28 15:01:56 +0300

    first commit bot - need add token and run console bot. Bot answer "Hello, <username>" for all text message

---
+++ pwserver_bot/pwserver_bot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace pwserver_bot
+{
+    //read me https://habrahabr.ru/post/316222/
+    class Program
+    {
+        private static string token = "<YOUR TOKEN>";
+        private static readonly TelegramBotClient bot = new TelegramBotClient(token);
+        static void Main(string[] args)
+        {
+            bot.OnMessage += Bot_OnMessage;
+            bot.SetWebhookAsync();
+
+            var me = bot.GetMeAsync().Result;
+            Console.Title = me.Username;
+
+            bot.StartReceiving();
+            Console.ReadLine();
+            bot.StopReceiving();
+        }
+
+        private static async void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Message msg = e.Message;
+
+            if (msg == null) return;
+
+            // Если сообщение текстовое
+            if (msg.Type == MessageType.TextMessage)
+            {
+                await bot.SendTextMessageAsync(msg.Chat.Id, "Hello, " + msg.From.FirstName + "!");
+            }
+        }
+    }
+}
