963edca7bdd2bda0b375e3ec2526d2d3cac1f5d6
Aliaksandr_Harbunou <Aliaksandr_Harbunou@epam.com>
2016-12-09 14:26:34 +0300

    delete service - bad idea use soap - only console app for test

--- pwserver_bot/pwserver_bot/Program.cs
+++ pwserver_bot/pwserver_bot/Program.cs
@@ -8,11 +8,11 @@ namespace pwserver_bot
     //read me https://habrahabr.ru/post/316222/
     class Program
     {
-        private static string token = "<YOUR TOKEN>";
+        private static string token = "Your Token";
         private static readonly TelegramBotClient bot = new TelegramBotClient(token);
         static void Main(string[] args)
-        {
-            bot.OnMessage += Bot_OnMessage;
+        {            
+            bot.OnMessage += Bot_OnMessage;            
             bot.SetWebhookAsync();
 
             var me = bot.GetMeAsync().Result;
@@ -32,7 +32,7 @@ namespace pwserver_bot
             // Если сообщение текстовое
             if (msg.Type == MessageType.TextMessage)
             {
-                await bot.SendTextMessageAsync(msg.Chat.Id, "Hello, " + msg.From.FirstName + "!");
+                await bot.SendTextMessageAsync(msg.Chat.Id, "Hello, " + msg.From.FirstName + msg.Text + " msgID" + msg.MessageId);
             }
         }
     }
